b0a6db1c0c40aad06b73846293526f6309733306
mehdi <mehdi_Adeli@aut.ac.ir>
2018-10-19 12:21:28 +0330

    complete login section

--- comp3095_Team_Sky_Assignment1/WebContent/login.jsp
+++ comp3095_Team_Sky_Assignment1/WebContent/login.jsp
@@ -1,51 +1,54 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+         pageEncoding="UTF-8" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>A web page</title>
-<script src="https://www.google.com/recaptcha/api.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>A web page</title>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
 </head>
 <body>
-	<div style="text-align: center;">
-		<div
-			style="text-align: center; display: inline-block; padding: 40px; border: 1px solid;">
-			<h2>Login</h2>
-			<br>
-			<p style="color: red"></p>
-			<!-- Show login message-->
-			<%
-				if (session.getAttribute("loginError") != null) {
-			%>
-			<span style="color: red;"><%=session.getAttribute("loginError")%></span>
-			<%
-				}
-			%>
-			<form action="Login" method="post">
-				<div>
-					<label for="username">Username: </label> <input type="text"
-						name="username">
-				</div>
-				<div>
-					<label for="password">Password:</label> <input type="password"
-						name="password">
-				</div>
-				<div class= "g-recaptcha" data-sitekey="6LeAhHMUAAAAANVM4Te-E_-Ol8ATN7NTXb_CqN0K"></div>
+<div style="text-align: center;">
+    <div
+            style="text-align: center; display: inline-block; padding: 40px; ">
+        <h2>LOGIN</h2>
+        <br>
+        <p style="color: red"></p>
+        <% if (session.getAttribute("username") != null) {response.sendRedirect("dashboard.jsp");}
+        %>
+        <!-- Show login message-->
+        <%
+            if (session.getAttribute("loginError") != null) {
+        %>
+        <span style="color: red;"><%=session.getAttribute("loginError")%></span>
+        <%
+                session.removeAttribute("loginError");
+            }
+        %>
+        <form action="Login" method="post">
+            <div>
+                <label>Username: </label> <input type="text"
+                                                 name="username">
+            </div>
+            <div>
+                <label>Password:</label> <input type="password"
+                                                name="password">
+            </div>
+            <div class="g-recaptcha" data-sitekey="6LeAhHMUAAAAANVM4Te-E_-Ol8ATN7NTXb_CqN0K"></div>
 
-				<br>
-				<div>
-					<input type="submit" name="submitLogin" value="Login"> <input
-						type="submit" name="gotoRegister" value="Register">
+            <br>
+            <div>
+                <input type="submit" name="submitLogin" value="Login"> <input
+                    type="submit" name="gotoRegister" value="Register">
 
-				</div>
-			</form>
-			<div>
-			<p/>
-				<a href="#">Forgot your password?</a>
-			</div>
-		</div>
-	</div>
+            </div>
+        </form>
+        <div>
+            <p/>
+            <a href="#">Forgot your password?</a>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
--- comp3095_Team_Sky_Assignment1/src/login_servlet/Login.java
+++ comp3095_Team_Sky_Assignment1/src/login_servlet/Login.java
@@ -10,12 +10,13 @@
 */
 package login_servlet;
 
+import helper.DataBaseHelper;
+import model.UserModel;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,61 +30,72 @@ public class Login extends HttpServlet {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		Cookie userName = new Cookie("userName", request.getParameter("username"));
-		userName.setMaxAge(60*60*24);
-		response.addCookie(userName);
-		
-		response.setContentType("text/html");
-		PrintWriter pw = response.getWriter();		
-		
-		String docType = "<!DOCTYPE html>";
-		String header = "<html><head><title>Insert title here</title>"
-						+ "<script src='https://www.google.com/recaptcha/api.js'></script>"
-						+ "</head></html><body><h1>LOGIN</h1>";		
-		String bodeyTags ="<filedset>"
-							+ "<form action='Login' method='POST'>"
-								+ "<lable for='username'>Username:</lable>"
-									+ "<input type='text' name='username'><br>"
-								+ "<lable for='password'>Password:</lable>"
-									+ "<input type='text' name='password'><br>"
-								+ "<div class=\"g-recaptcha\" data-sitekey=\"6LdUknMUAAAAAMbLTVnxfmyvpVnr-sAcWQ5UIN8P\"></div>"
-									+ "<input type='submit' value='Login'>"
-									+ "<input type='submit' value='Register' formaction='Register'><br>"
-									+ "<a href='./test.html'>Forgot your password?</a>"
-							+ "</form>"
-						+ "</filedset>";		
-		String footer = "</body></html>";	
-		String htmlTags = docType + header + bodeyTags + footer;
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getParameter("logout") != null) {
+			request.getSession().removeAttribute("username");
 
-			pw.println(htmlTags);
-			
-		
-		
+			response.sendRedirect("login.jsp");
+			request.getSession().invalidate();
+
+			return;
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String adminUserName = "admin@isp.net";
 		String adminPassword = "P@ssword1";
-
+		
+		if(request.getParameter("gotoRegister")!=null && request.getParameter("gotoRegister").equals("Register")) {
+			response.sendRedirect("register.jsp");
+			return;
+		}
+		
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
 		boolean verify =helper.Helper.verifyRecaptcha(gRecaptchaResponse);
-		PrintWriter out = response.getWriter();
-//		if (verify) {
-//			out.println("<font color=red>Either user name or password is wrong.</font>");
-//		} else {
-//			out.println("<font color=red>You missed the Captcha.</font>");
-//		}
-		if(helper.Helper.isEmty(username) && helper.Helper.isEmty(password)) {
-			if(username.equals(adminUserName) && password.equals(adminPassword)) {
-				response.sendRedirect("index.html");
+		
+		if (!verify) {
+			request.getSession().setAttribute("loginError", "Recaptcha Verification Failed");
+			response.sendRedirect("login.jsp");
+			return;
+		}
+		DataBaseHelper dbHelper=new DataBaseHelper();
+		if(helper.Helper.isNotEmty(username) && helper.Helper.isNotEmty(password)) {
+
+			if(username.equals(adminUserName) && password.equals(adminPassword) ) {
+				request.getSession().setAttribute("firstname","admin");
+				request.getSession().setAttribute("lastname","admin");
+				request.getSession().setAttribute("username","admin@isp.net");
+				request.getSession().setAttribute("role","admin");
+
+				response.sendRedirect("dashboard.jsp");
+				return;
+			}
+
+			else {
+				UserModel userFromDataBase = dbHelper.getUserFromDataBase(username, password);
+				if (userFromDataBase==null) {
+					request.getSession().setAttribute("loginError", "Username or Password is wrong!");
+					response.sendRedirect("login.jsp");
+					return;
+				}else {
+					request.getSession().setAttribute("firstname",userFromDataBase.getFirstName());
+					request.getSession().setAttribute("lastname",userFromDataBase.getLastName());
+					request.getSession().setAttribute("username",userFromDataBase.getEmail());
+					request.getSession().setAttribute("role",userFromDataBase.getRole());
+
+					response.sendRedirect("dashboard.jsp");
+					return;
+				}
 			}
 		}
+		else {
+			request.getSession().setAttribute("loginError", "Username or Password required!");
+			response.sendRedirect("login.jsp");
+			return;
+		}
+		
 	}
 
 }
